6ad2808f1905acb993093a36b52429957ca7ae4d
Prerana Bhat <preranabhat8@gmail.com>
2021-06-26 12:22:49 +0530

    hav added commit

--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+
+
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -13,9 +18,12 @@ def store(request, category_slug=None):
         product_count = products.count()
     else:
         products=Product.objects.all().filter(is_available=True)
+        paginator=Paginator(products,1)
+        page=request.GET.get('page')
+        paged_products=paginator.get_page(page)
         product_count=products.count()
     context={
-        'products':products,
+        'products':paged_products,
         'product_count':product_count,
     }
     return render(request, 'store/store.html',context)
@@ -23,10 +31,13 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        in_cart=CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
+
     except Exception as e:
         raise e
     context = {
         'single_product':single_product,
+        'in_cart':in_cart,
     }
 
 
